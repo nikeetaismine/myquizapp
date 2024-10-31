ef6500f07a54b80549e74c3bb3bcb0f0c46d56f6
Denise <denisenmwangi@gmail.com>
2024-10-31 12:43:12 +0300

    First commit of a fun developer quiz

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import QuizCard from '@/components/QuizCard.vue';
+
+
+</script>
+
+<template>
+  <div class="container">
+    <QuizCard />
+  </div>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap');
+
+*{
+  box-sizing: border-box;
+}
+
+
+.container {
+  background: #fffbd5;  /* fallback for old browsers */
+  background: -webkit-linear-gradient(to right, #b20a2c, #fffbd5);  /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to right, #b20a2c, #fffbd5); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ src/components/QuizCard.vue
@@ -0,0 +1,82 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const questions = ref ([
+    {
+        'question': "Which JavaScript data type can hold multiple values?",
+        'answer': ["Array", "Number", "Object", "Boolean"],
+        'correctAnswer': "Object"
+    },
+    {
+        'question': "What will `typeof NaN` return?",
+        'answer': ["'undefined'", "'number'", "'NaN'", "'object'"],
+        'correctAnswer': "'number'"
+    },
+    {
+        'question': "What is the output of `console.log([] + [])` in JavaScript?",
+        'answer': ["[]", "'undefined'", "0", "'' (empty string)"],
+        'correctAnswer': "'' (empty string)"
+    },
+    {
+        'question': "Which of these is a valid way to declare a variable in JavaScript?",
+        'answer': ["var", "let", "const", "All of the above"],
+        'correctAnswer': "All of the above"
+    },
+    {
+        'question': "What will `console.log(0.1 + 0.2 === 0.3)` output?",
+        'answer': ["true", "false", "undefined", "NaN"],
+        'correctAnswer': "false"
+    },
+    {
+        'question': "What is the result of `typeof null` in JavaScript?",
+        'answer': ["'object'", "'null'", "'undefined'", "'boolean'"],
+        'correctAnswer': "'object'"
+    },
+    {
+        'question': "What will `console.log([] == ![])` output?",
+        'answer': ["true", "false", "undefined", "NaN"],
+        'correctAnswer': "true"
+    },
+    {
+        'question': "What does the following function return? `function f() {} return [typeof f];`",
+        'answer': ["'function'", "'undefined'", "'null'", "'object'"],
+        'correctAnswer': "'function'"
+    },
+    {
+        'question': "Which statement will correctly catch an error in JavaScript?",
+        'answer': ["catch(error)", "try {}", "try {} catch {}", "catch {} try {}"],
+        'correctAnswer': "try {} catch {}"
+    },
+    {
+        'question': "How many times does this loop run? `for (let i = 0; i < 5; i += 2) {}`",
+        'answer': ["5 times", "3 times", "2 times", "Infinite times"],
+        'correctAnswer': "3 times"
+    }
+]);
+
+
+</script>
+
+<template>
+    <div class="wrapper">
+
+        <form action="">
+
+            <div>
+                <input type="">
+                <label for=""></label>
+            </div>
+        </form>
+    </div>
+
+</template>
+
+<style scoped>
+
+.wrapper {
+    height: 300px;
+    width: 400px;
+    background-color: #fff;
+    border-radius: 10px;
+}
+</style>
